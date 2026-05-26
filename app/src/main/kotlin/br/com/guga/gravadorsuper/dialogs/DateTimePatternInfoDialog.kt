e1c5d35ddcfc9a1dcfd87f37ffe5fc99a783597b
miolanatan9-debug <miolanatan9@gmail.com>
2026-05-26 17:34:19 -0300

    Pushing app/src/main/kotlin/br/com/guga/gravadorsuper/dialogs/DateTimePatternInfoDialog.kt from repo_final

---
+++ app/src/main/kotlin/br/com/guga/gravadorsuper/dialogs/DateTimePatternInfoDialog.kt
@@ -0,0 +1,19 @@
+package br.com.guga.gravadorsuper.dialogs
+
+import org.fossify.commons.activities.BaseSimpleActivity
+import org.fossify.commons.extensions.getAlertDialogBuilder
+import org.fossify.commons.extensions.setupDialogStuff
+import org.fossify.commons.extensions.viewBinding
+import br.com.guga.gravadorsuper.databinding.DatetimePatternInfoLayoutBinding
+
+class DateTimePatternInfoDialog(activity: BaseSimpleActivity) {
+    val binding by activity.viewBinding(DatetimePatternInfoLayoutBinding::inflate)
+
+    init {
+        activity.getAlertDialogBuilder()
+            .setPositiveButton(org.fossify.commons.R.string.ok) { _, _ -> { } }
+            .apply {
+                activity.setupDialogStuff(binding.root, this)
+            }
+    }
+}
