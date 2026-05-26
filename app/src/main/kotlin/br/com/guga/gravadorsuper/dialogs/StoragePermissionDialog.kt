ef5180798a1b39f3c6262107109f1285ac5ef8db
miolanatan9-debug <miolanatan9@gmail.com>
2026-05-26 17:34:20 -0300

    Pushing app/src/main/kotlin/br/com/guga/gravadorsuper/dialogs/StoragePermissionDialog.kt from repo_final

---
+++ app/src/main/kotlin/br/com/guga/gravadorsuper/dialogs/StoragePermissionDialog.kt
@@ -0,0 +1,34 @@
+package br.com.guga.gravadorsuper.dialogs
+
+import androidx.appcompat.app.AlertDialog
+import org.fossify.commons.activities.BaseSimpleActivity
+import org.fossify.commons.databinding.DialogMessageBinding
+import org.fossify.commons.extensions.getAlertDialogBuilder
+import org.fossify.commons.extensions.setupDialogStuff
+import br.com.guga.gravadorsuper.R
+
+class StoragePermissionDialog(
+    private val activity: BaseSimpleActivity,
+    private val callback: (result: Boolean) -> Unit
+) {
+    private var dialog: AlertDialog? = null
+
+    init {
+        val view = DialogMessageBinding.inflate(activity.layoutInflater, null, false)
+        view.message.text = activity.getString(R.string.confirm_recording_folder)
+
+        activity.getAlertDialogBuilder()
+            .setPositiveButton(org.fossify.commons.R.string.ok) { _, _ ->
+                callback(true)
+            }
+            .apply {
+                activity.setupDialogStuff(
+                    view = view.root,
+                    dialog = this,
+                    cancelOnTouchOutside = false,
+                ) { alertDialog ->
+                    dialog = alertDialog
+                }
+            }
+    }
+}
