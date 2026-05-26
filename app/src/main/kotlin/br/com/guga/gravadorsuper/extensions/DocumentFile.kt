fb3cc6ee289a95cedc70b77386a095022ddb242e
miolanatan9-debug <miolanatan9@gmail.com>
2026-05-26 17:32:47 -0300

    Pushing app/src/main/kotlin/br/com/guga/gravadorsuper/extensions/DocumentFile.kt from repo_final

---
+++ app/src/main/kotlin/br/com/guga/gravadorsuper/extensions/DocumentFile.kt
@@ -0,0 +1,11 @@
+package br.com.guga.gravadorsuper.extensions
+
+import androidx.documentfile.provider.DocumentFile
+
+fun DocumentFile.isAudioRecording(): Boolean {
+    return type.isAudioMimeType() && !name.isNullOrEmpty() && !name!!.startsWith(".")
+}
+
+fun DocumentFile.isTrashedMediaStoreRecording(): Boolean {
+    return type.isAudioMimeType() && !name.isNullOrEmpty() && name!!.startsWith(".trashed-")
+}
