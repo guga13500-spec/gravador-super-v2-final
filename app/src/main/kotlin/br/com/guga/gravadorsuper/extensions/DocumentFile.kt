c451a765e72b8c9fedb7ee6356be9f1180e5625b
miolanatan9-debug <miolanatan9@gmail.com>
2026-05-26 20:51:20 -0300

    Pushing app/src/main/kotlin/br/com/guga/gravadorsuper/extensions/DocumentFile.kt from repo_final

--- app/src/main/kotlin/br/com/guga/gravadorsuper/extensions/DocumentFile.kt
+++ app/src/main/kotlin/br/com/guga/gravadorsuper/extensions/DocumentFile.kt
@@ -2,10 +2,14 @@ package br.com.guga.gravadorsuper.extensions
 
 import androidx.documentfile.provider.DocumentFile
 
+import java.util.Locale
+
 fun DocumentFile.isAudioRecording(): Boolean {
-    return type.isAudioMimeType() && !name.isNullOrEmpty() && !name!!.startsWith(".")
+    val name = name?.lowercase(Locale.ROOT) ?: return false
+    if (name.startsWith(".") || name.isEmpty()) return false
+    return type?.startsWith("audio") == true || name.endsWith(".wav") || name.endsWith(".m4a")
 }
 
 fun DocumentFile.isTrashedMediaStoreRecording(): Boolean {
-    return type.isAudioMimeType() && !name.isNullOrEmpty() && name!!.startsWith(".trashed-")
+    return name?.startsWith(".trashed") == true
 }
