337de0843e39985131b3fb57a2bd44d195fb7789
miolanatan9-debug <miolanatan9@gmail.com>
2026-05-26 17:34:24 -0300

    Pushing app/src/main/kotlin/br/com/guga/gravadorsuper/helpers/Config.kt from repo_final

---
+++ app/src/main/kotlin/br/com/guga/gravadorsuper/helpers/Config.kt
@@ -0,0 +1,105 @@
+package br.com.guga.gravadorsuper.helpers
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.media.MediaRecorder
+import androidx.core.content.edit
+import org.fossify.commons.helpers.BaseConfig
+import br.com.guga.gravadorsuper.R
+import br.com.guga.gravadorsuper.extensions.getDefaultRecordingsFolder
+
+class Config(context: Context) : BaseConfig(context) {
+    companion object {
+        fun newInstance(context: Context) = Config(context)
+    }
+
+    var saveRecordingsFolder: String
+        get() = prefs.getString(SAVE_RECORDINGS, context.getDefaultRecordingsFolder())!!
+        set(saveRecordingsFolder) = prefs.edit().putString(SAVE_RECORDINGS, saveRecordingsFolder)
+            .apply()
+
+    var extension: Int
+        get() = prefs.getInt(EXTENSION, EXTENSION_M4A)
+        set(extension) = prefs.edit().putInt(EXTENSION, extension).apply()
+
+    var microphoneMode: Int
+        get() = prefs.getInt(MICROPHONE_MODE, MediaRecorder.AudioSource.DEFAULT)
+        set(audioSource) = prefs.edit { putInt(MICROPHONE_MODE, audioSource) }
+
+    fun getMicrophoneModeText(mode: Int) = context.getString(
+        when (mode) {
+            MediaRecorder.AudioSource.CAMCORDER -> R.string.microphone_mode_camcorder
+            MediaRecorder.AudioSource.VOICE_COMMUNICATION -> R.string.microphone_mode_voice_communication
+            MediaRecorder.AudioSource.VOICE_PERFORMANCE -> R.string.microphone_mode_voice_performance
+            MediaRecorder.AudioSource.VOICE_RECOGNITION -> R.string.microphone_mode_voice_recognition
+            MediaRecorder.AudioSource.UNPROCESSED -> R.string.microphone_mode_unprocessed
+            else -> org.fossify.commons.R.string.system_default
+        }
+    )
+
+    var bitrate: Int
+        get() = prefs.getInt(BITRATE, DEFAULT_BITRATE)
+        set(bitrate) = prefs.edit().putInt(BITRATE, bitrate).apply()
+
+    var samplingRate: Int
+        get() = prefs.getInt(SAMPLING_RATE, DEFAULT_SAMPLING_RATE)
+        set(samplingRate) = prefs.edit().putInt(SAMPLING_RATE, samplingRate).apply()
+
+    var recordAfterLaunch: Boolean
+        get() = prefs.getBoolean(RECORD_AFTER_LAUNCH, false)
+        set(recordAfterLaunch) = prefs.edit().putBoolean(RECORD_AFTER_LAUNCH, recordAfterLaunch)
+            .apply()
+
+    fun getExtensionText() = context.getString(
+        when (extension) {
+            EXTENSION_M4A -> R.string.m4a
+            EXTENSION_OGG -> R.string.ogg_opus
+            EXTENSION_WAV -> R.string.wav_pcm
+            else -> R.string.mp3_experimental
+        }
+    )
+
+    fun getExtension() = context.getString(
+        when (extension) {
+            EXTENSION_M4A -> R.string.m4a
+            EXTENSION_OGG -> R.string.ogg
+            EXTENSION_WAV -> R.string.wav
+            else -> R.string.mp3
+        }
+    )
+
+    @SuppressLint("InlinedApi")
+    fun getOutputFormat() = when (extension) {
+        EXTENSION_OGG -> MediaRecorder.OutputFormat.OGG
+        else -> MediaRecorder.OutputFormat.MPEG_4
+    }
+
+    @SuppressLint("InlinedApi")
+    fun getAudioEncoder() = when (extension) {
+        EXTENSION_OGG -> MediaRecorder.AudioEncoder.OPUS
+        else -> MediaRecorder.AudioEncoder.AAC
+    }
+
+    var useRecycleBin: Boolean
+        get() = prefs.getBoolean(USE_RECYCLE_BIN, true)
+        set(useRecycleBin) = prefs.edit().putBoolean(USE_RECYCLE_BIN, useRecycleBin).apply()
+
+    var lastRecycleBinCheck: Long
+        get() = prefs.getLong(LAST_RECYCLE_BIN_CHECK, 0L)
+        set(lastRecycleBinCheck) = prefs.edit().putLong(LAST_RECYCLE_BIN_CHECK, lastRecycleBinCheck)
+            .apply()
+
+    var keepScreenOn: Boolean
+        get() = prefs.getBoolean(KEEP_SCREEN_ON, true)
+        set(keepScreenOn) = prefs.edit().putBoolean(KEEP_SCREEN_ON, keepScreenOn).apply()
+
+    var wasMicModeWarningShown: Boolean
+        get() = prefs.getBoolean(WAS_MIC_MODE_WARNING_SHOWN, false)
+        set(wasMicModeWarningShown) = prefs.edit {
+            putBoolean(WAS_MIC_MODE_WARNING_SHOWN, wasMicModeWarningShown)
+        }
+
+    var filenamePattern: String
+        get() = prefs.getString(FILENAME_PATTERN, DEFAULT_FILENAME_PATTERN)!!
+        set(filenamePattern) = prefs.edit { putString(FILENAME_PATTERN, filenamePattern) }
+}
